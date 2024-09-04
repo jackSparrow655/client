fd1534beb26860c97e506e5dddec29026f0bc092
Arijit655 <arijitbarik655@gmail.com>
2024-09-04 22:05:04 +0530

    first commit

---
+++ src/redux/features/auth/authSlice.js
@@ -0,0 +1,69 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getCurrentUser, userLogin, userRegister } from "./authActions";
+
+const token = localStorage.getItem("token")
+  ? localStorage.getItem("token")
+  : null;
+
+const initialState = {
+  loading: false,
+  user: null,
+  token,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // login user
+    builder.addCase(userLogin.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(userLogin.fulfilled, (state, { payload }) => {
+      console.log(payload)
+      state.loading = false;
+      state.user = payload.user;
+      state.token = payload.token;
+    });
+    builder.addCase(userLogin.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+      state.user = null
+      state.token = null
+    });
+    // REGISTER user
+    builder.addCase(userRegister.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(userRegister.fulfilled, (state, { payload }) => {
+      console.log(payload)
+      state.loading = false;
+      state.user = payload.user
+      state.token = payload.token
+    });
+    builder.addCase(userRegister.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+    // CURRENT user
+    builder.addCase(getCurrentUser.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(getCurrentUser.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user;
+    });
+    builder.addCase(getCurrentUser.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+      state.token = null
+    });
+  },
+});
+
+export default authSlice;
